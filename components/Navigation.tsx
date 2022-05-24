ff20ac92f029c7983acc2785e14c222c4b297192
Joey <31454088+itsmejoeyb@users.noreply.github.com>
2022-05-24 13:54:06 -0400

    various changes

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -91,11 +91,7 @@ const Navigation = ({ children }:Props) => {
                                         </div>
                                     </Transition.Child>
                                     <div className="flex-shrink-0 flex items-center px-4">
-                                        {/* <img
-                                            className="h-8 w-auto"
-                                            src="https://tailwindui.com/img/logos/workflow-logo-cyan-300-mark-white-text.svg"
-                                            alt="Workflow"
-                                        /> */}
+                                        {/* logo here */}
                                     </div>
                                     <div className="mt-5 flex-1 h-0 overflow-y-auto">
                                         <nav className="px-2 space-y-1">
@@ -125,14 +121,10 @@ const Navigation = ({ children }:Props) => {
 
                 {/* Static sidebar for desktop */}
                 <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
-                    {/* Sidebar component, swap this element with another sidebar if you like */}
+                    {/* Sidebar component */}
                     <div className="flex flex-col flex-grow pt-5 bg-cyan-700 overflow-y-auto">
                         <div className="flex items-center flex-shrink-0 px-4">
-                            {/* <img
-                                className="h-8 w-auto"
-                                src="https://tailwindui.com/img/logos/workflow-logo-cyan-300-mark-white-text.svg"
-                                alt="Workflow"
-                            /> */}
+                            {/* logo */}
                         </div>
                         <div className="mt-5 flex-1 flex flex-col">
                             <nav className="flex-1 px-2 pb-4 space-y-1">
--- pages/workorders/[slug].tsx
+++ pages/workorders/[slug].tsx
@@ -238,7 +238,7 @@ const WorkOrder: NextPage<Props> = (props) => {
                               id="comment"
                               name="comment"
                               rows={3}
-                              className="shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 sm:text-sm border border-gray-300 rounded-md"
+                              className="shadow-sm block w-full focus:ring-cyan-500 focus:border-cyan-500 sm:text-sm border border-gray-300 rounded-md"
                               placeholder="Add a note"
                               defaultValue={''}
                             />
@@ -296,14 +296,6 @@ const WorkOrder: NextPage<Props> = (props) => {
                         </div>
                     </fieldset>
                 </div>
-                {/* <div className="mt-6 flex flex-col justify-stretch">
-                  <button
-                    type="button"
-                    className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                  >
-                    Advance to offer
-                  </button>
-                </div> */}
               </div>
             </section>
           </div>
