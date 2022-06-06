5ebe249882a58e95a5c22500e8a4632a354aad9c
Joey <31454088+itsmejoeyb@users.noreply.github.com>
2022-06-06 15:13:40 -0400

    fixed prop names

--- pages/workorders/[slug].tsx
+++ pages/workorders/[slug].tsx
@@ -260,7 +260,7 @@ const WorkOrder: NextPage<Props> = (props) => {
                 <div className="px-4 py-6 sm:px-6">
                   <ul role="list" className="space-y-8">
                     {imageUrls.length > 0 ? imageUrls.map((imageSrc) => (
-                      <li>
+                      <li key={imageSrc}>
                         <img src={imageSrc} className="w-full h" />
                       </li>
                     )) : 
@@ -276,7 +276,7 @@ const WorkOrder: NextPage<Props> = (props) => {
                           viewBox="0 0 24 24"
                           aria-hidden="true"
                         >
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                         </svg>
                         <span className="mt-2 block text-sm font-medium text-gray-600">Add photos</span>
                         <input type="file" multiple accept='image/*' id="images" className="hidden" onChange={onImageChange} />
